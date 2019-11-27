e94d5fc8c7be2a964c97b0bfcb99cc8948c67bcb
Yi Zhang <yizhang82@outlook.com>
2019-11-27 14:38:34 -0800

    Update install.sh and fix_audio.sh

--- sys/fix_audio.sh
+++ sys/fix_audio.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo apt install alsa-tools
-hda-verb /dev/snd/hwC0D0 0x1d SET_PIN_WIDGET_CONTROL 0x0
+sudo hda-verb /dev/snd/hwC0D0 0x1d SET_PIN_WIDGET_CONTROL 0x0
--- sys/install.sh
+++ sys/install.sh
@@ -3,6 +3,9 @@
 # Developer Tools
 sudo apt install autoconf cmake
 
+# Libraries
+sudo apt install libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev
+
 # Languages
 sudo apt install golang rustc
 
