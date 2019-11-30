df1df9297da8306c24c585c875b51911d19d44c3
Yi Zhang <yizhang82@outlook.com>
2019-11-29 16:38:50 -0800

    Fix libsecret installation

--- sys/setup_git_keyring_libsecret.sh
+++ sys/setup_git_keyring_libsecret.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+sudo apt-get install libsecret-1-0 libsecret-1-dev
 cd /usr/share/doc/git/contrib/credential/libsecret
 sudo make
 git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
