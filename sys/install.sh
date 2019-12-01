a9435411840d4815923bca9be2e286e61997a64b
Yi Zhang <yizhang82@outlook.com>
2019-11-30 21:36:19 -0800

    Update install.sh to include more libraries

--- sys/install.sh
+++ sys/install.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+# System
+sudo apt install tlp
+
 # Developer Tools
-sudo apt install autoconf cmake
+sudo apt install autoconf cmake lldb bison
 
 # Libraries
-sudo apt install libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev
+sudo apt install libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libssl-dev libzstd-dev libncurses5-dev libreadline-dev
 
 # Languages
-sudo apt install golang rustc
+sudo apt install golang rustc python
 
 # Utilities
-sudo apt install curl
+sudo apt install curl silversearcher-ag pkg-config
+
