65dc135c670257630d2f5e7f93a844a2a788b6ef
Yi Zhang <yizhang82@outlook.com>
2019-11-30 21:36:56 -0800

    Add clone_src.sh

---
+++ sys/clone_src.sh
@@ -0,0 +1,16 @@
+# Clone interesting projects
+
+# Database projects
+git clone https://github.com/facebook/rocksdb
+git clone https://github.com/mysql/mysql-server
+git clone https://github.com/google/leveldb
+git clone https://github.com/postgres/postgres
+
+
+# Blockchain
+git clone https://github.com/bitcoin/bitcoin
+
+# OS
+git clone https://github.com/torvalds/linux
+
+
