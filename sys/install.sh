6a92ca0c595aec16b7feb56deb8abb5e6b1e1c6f
Yi Zhang <yizhang82@outlook.com>
2019-12-04 21:40:35 -0800

    Update install.sh to install without asking for confirmation

--- sys/install.sh
+++ sys/install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # System
-sudo apt install tlp
+sudo apt install tlp -y
 
 # Developer Tools
-sudo apt install autoconf cmake lldb bison
+sudo apt install autoconf cmake lldb bison -y
 
 # Libraries
-sudo apt install libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libssl-dev libzstd-dev libncurses5-dev libreadline-dev
-sudo apt install libsqlite3-dev
+sudo apt install libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libssl-dev libzstd-dev libncurses5-dev libreadline-dev -y
+sudo apt install libsqlite3-dev -y
 
 # Languages
-sudo apt install golang rustc python
+sudo apt install golang rustc python -y
 
 # Utilities
-sudo apt install curl silversearcher-ag pkg-config
+sudo apt install curl silversearcher-ag pkg-config -y
 
