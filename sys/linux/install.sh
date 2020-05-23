402c0b8b5b545887ff3de1ab9a0a65e37fc8735d
Yi Zhang <yizhang82@outlook.com>
2020-05-22 19:01:16 -0700

    Add clang installation

--- sys/linux/install.sh
+++ sys/linux/install.sh
@@ -10,6 +10,7 @@ sudo apt install autoconf cmake lldb bison -y
 # Libraries
 sudo apt install libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libssl-dev libzstd-dev libncurses5-dev libreadline-dev -y
 sudo apt install libsqlite3-dev -y
+sudo apt install clang -y
 
 # Languages
 sudo apt install golang rustc python -y
