44814b69b3d0bc50a281f0a9096f381a4c32b4f6
Yi Zhang <yizhang82@outlook.com>
2020-05-24 12:11:52 -0700

    Update clone_src sh for personal projects

--- sys/linux/clone_src.sh
+++ sys/linux/clone_src.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
 # Clone interesting projects
+
+git clone https://github.com/yizhang82/dotfiles ~/dotfiles
+
+mkdir -p ~/github
+pushd ~/github
+
+# Personal repos
+git clone https://github.com/yizhang82/utils
+git clone https://github.com/yizhang82/yizhang82.github.io
 
 # Database projects
 git clone https://github.com/facebook/rocksdb
@@ -6,11 +16,10 @@ git clone https://github.com/mysql/mysql-server
 git clone https://github.com/google/leveldb
 git clone https://github.com/postgres/postgres
 
-
 # Blockchain
 git clone https://github.com/bitcoin/bitcoin
 
 # OS
 git clone https://github.com/torvalds/linux
 
-
+popd
