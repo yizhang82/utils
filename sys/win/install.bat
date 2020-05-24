50f5fc34e630c40be3fa8aa43e4dc5f6460dcbf9
Yi Zhang <yizhang82@outlook.com>
2020-05-23 17:32:00 -0700

    Add windows scripts

---
+++ sys/win/install.bat
@@ -0,0 +1,4 @@
+choco install rust -y 
+choco install python -y
+choco install golang -y
+choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' -y
---
+++ sys/win/setup.bat
@@ -0,0 +1,2 @@
+git config --global user.email "yizhang82@outlook.com"
+git config --global user.name "Yi Zhang" 
