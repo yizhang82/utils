28fe8d412b85131e63d047cfe34de6571da2df10
Yi Zhang <yizhang82@outlook.com>
2020-05-24 11:42:30 -0700

    Update install.sh for WSL

--- sys/linux/install.sh
+++ sys/linux/install.sh
@@ -5,6 +5,7 @@ sudo apt install tlp powerstat -y
 sudo apt install qemu -y
 
 # Developer Tools
+sudo apt install make -y
 sudo apt install autoconf cmake lldb bison -y
 
 # Libraries
@@ -19,10 +20,10 @@ sudo apt install golang rustc python -y
 sudo apt install curl silversearcher-ag pkg-config -y
 
 # Docker
-sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install docker-ce docker-ce-cli containerd.io -y
