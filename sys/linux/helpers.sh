6e3c749c822bf0466377e58e2cb504827af22f03
Yi Zhang <yizhang82@outlook.com>
2020-12-28 18:37:06 -0800

    Fedora support

--- sys/linux/helpers.sh
+++ sys/linux/helpers.sh
@@ -1,22 +1,53 @@
+PKG_TYPE=
+if [ -x "$(command -v apt-get)" ]; then
+  PKG_TYPE="apt"
+elif [ -x "$(command -v brew)" ]; then
+  PKG_TYPE="brew"
+
+elif [ -x "$(command -v pkg)" ]; then
+  PKG_TYPE="pkg"
+
+elif [ -x "$(command -v pacman)" ]; then
+  PKG_TYPE="pacman"
+
+elif [ -x "$(command -v dnf)" ]; then
+  PKG_TYPE="dnf"
+
+else
+  echo "Unknown package manager"
+  exit -1
+fi
+
 install_pkg() {
-    # This could def use community support
-    if [ -x "$(command -v apt-get)" ]; then
-        sudo apt-get install $1 -y
+  if [ "$PKG_TYPE" == "apt" ] ; then
+    sudo apt-get install $* -y
+  elif [ "$PKG_TYPE" == "brew" ]; then
+    brew install $*
+  elif [ "$PKG_TYPE" == "pkg" ]; then
+    sudo pkg install $*
+  elif [ "$PKG_TYPE" == "pacman" ]; then
+    sudo pacman -S $*
+  elif [ "$PKG_TYPE" == "dnf" ]; then
+    sudo dnf install -y $*
+  else
+    echo "Unknown package manager!"
+    exit -1
+  fi
+}
 
-    elif [ -x "$(command -v brew)" ]; then
-        brew install $1
-
-    elif [ -x "$(command -v pkg)" ]; then
-        sudo pkg install $1
-
-    elif [ -x "$(command -v pacman)" ]; then
-        sudo pacman -S $1
-
-    elif [ -x "$(command -v dnf)" ]; then
-        sudo dnf install -y $1
-
-    else
-        echo "Unknown package manager"
-        exit -1
-    fi
+check_update_pkg() {
+  if [ "$PKG_TYPE" == "apt" ] ; then
+    sudo apt update
+  elif [ "$PKG_TYPE" == "brew" ]; then
+    echo "check_update_pkg: not suppported"
+  elif [ "$PKG_TYPE" == "pkg" ]; then
+    echo "check_update_pkg: not suppported"
+  elif [ "$PKG_TYPE" == "pacman" ]; then
+    echo "check_update_pkg: not suppported"
+  elif [ "$PKG_TYPE" == "dnf" ]; then
+    sudo dnf check-update
+  else
+    echo "Unknown package manager!"
+    exit -1
+  fi
 }
--- sys/linux/install.sh
+++ sys/linux/install.sh
@@ -1,36 +1,63 @@
 #!/bin/bash
 
-# Ensure apt database is up-to-date
-sudo apt update
+# Initialize
+source ./helpers.sh
+
+# Ensure package manager database is up-to-date
+echo "========================================"
+echo "Updating database..."
+echo "========================================"
+check_update_pkg
 
 # System
-sudo apt-get install sysstat -y
+echo "========================================"
+echo "Installing system utilities..."
+echo "========================================"
+install_pkg sysstat
 if [[ -z "$WSLENV" ]]; then
   # Only install following for native linux (not WSL)
-  sudo apt install tlp powerstat -y
-  sudo apt install qemu -y
+  install_pkg tlp
+  install_pkg powerstat
+  install_pkg qemu
 fi
 
 # Developer Tools
-sudo apt install make -y
-sudo apt install autoconf cmake lldb bison -y
-sudo apt install clang -y
+echo "========================================"
+echo "Installing developer utilities"
+echo "========================================"
+
+install_pkg make autoconf cmake lldb bison
+install_pkg gcc clang gdb lldb
 
 # Libraries
-sudo apt install libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libssl-dev libzstd-dev libncurses5-dev libreadline-dev -y
-sudo apt install libsqlite3-dev -y
+echo "========================================"
+echo "Installing developer utilities"
+echo "========================================"
+install_pkg libevent-dev libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libssl-dev libzstd-dev libncurses5-dev libreadline-dev
+install_pkg libsqlite3-dev
 
 # Languages
-sudo apt install golang rustc python -y
+echo "========================================"
+echo "Installing languages..."
+echo "========================================"
+install_pkg golang rustc cargo python
 
 # Utilities
-sudo apt install curl silversearcher-ag pkg-config -y
+echo "========================================"
+echo "Installing utilities..."
+echo "========================================"
+install_pkg curl silversearcher-ag pkg-config
 
 # Docker
-sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
-echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io -y
+echo "========================================"
+echo "Installing Docker..."
+echo "========================================"
+if [ "$PKG_TYPE" == "apt" ]; then
+  sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+  echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/docker.list
+  sudo apt update
+  sudo apt install docker-ce docker-ce-cli containerd.io -y
+fi
